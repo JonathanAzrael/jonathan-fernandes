026010b5768d6b7a548cd1dbaf5c2a78f885b432
Jonathan <designer.azrael@gmail.com>
2024-08-29 16:53:49 -0300

    .

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -7,7 +7,9 @@ import Whatsapp from "../assets/whatsapp.svg"
 export function SideBar() {
     return(
         <div className="flex flex-col gap-14 absolute h-screen flex items-center justify-center ml-8">
-            <Image alt="" src={Github} className="w-[50px] hover:scale-150 opacity-50 hover:opacity-100" />
+            <a href="https://github.com/JonathanAzrael">
+                <Image alt="" src={Github} className="w-[50px] hover:scale-150 opacity-50 hover:opacity-100" />
+            </a>
             <Image alt="" src={Email} className="w-[50px] hover:scale-150 opacity-50 hover:opacity-100" />
             <Image alt="" src={Whatsapp} className="w-[50px] hover:scale-150 opacity-50 hover:opacity-100 " />
         </div>
