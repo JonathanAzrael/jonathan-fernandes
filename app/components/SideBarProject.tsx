1002b9e2dbf70d3f6351d5a538a3afb1eaece775
Jonathan <designer.azrael@gmail.com>
2024-09-05 12:00:06 -0300

    .

--- app/components/SideBarProject.tsx
+++ app/components/SideBarProject.tsx
@@ -6,14 +6,14 @@ export function SideProject() {
             <h1 className="text-bgOrange font-semibold text-lg">Graphic Design</h1>
             <div className="flex flex-col font-medium gap-2">
                 <a href="/Project/Logo" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Logo</a>
-                <a href="/" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Flyer</a>
+                <a href="/Project" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Flyer</a>
                 <a href="/Project/Mockup" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Mockup</a>
             </div>
             <h2 className="text-bgOrange font-semibold text-lg">Web Developer</h2>
             <div className="flex flex-col font-medium gap-2">
-                <a href="/" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Landing Page</a>
-                <a href="/" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Mobile</a>
-                <a href="/" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">System</a>
+                <a href="/Project" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Landing Page</a>
+                <a href="/Project" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">Mobile</a>
+                <a href="/Project" className="w-full p-2 border-b-2 border-bgOrange hover:text-white hover:bg-bgOrange">System</a>
             </div>
         </div>
     )
